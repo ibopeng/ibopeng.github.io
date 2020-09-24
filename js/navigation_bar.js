7d7d98ee99e2182bd05793707cf56bc3ecaa438a
Bo Peng <31286663+ibopeng@users.noreply.github.com>
2020-09-24 11:39:28 -0500

    menu bar

--- js/navigation_bar.js
+++ js/navigation_bar.js
@@ -1,8 +1,15 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+function navDropdrown() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+		} else {
+		x.className = "topnav";
+	}
+
+	var y = document.getElementById("menu_icon");
+	if (x.className === "topnav") {
+		y.innerHTML = "&#xe5d2;"
+	} else {
+		y.innerHTML = "&#xe5cd;"
+	}
 }
